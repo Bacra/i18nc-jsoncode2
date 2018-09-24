a220be6cb25d59082dca0c337df928b6deeb35b8
Bacra <bacra.woo@gmail.com>
2018-09-25 00:57:49 +0800

    Update: clear $ of empty lans

--- test/output/generator/func_json.js
+++ test/output/generator/func_json.js
@@ -3,6 +3,7 @@ module.exports = function textWrapCode(){
 var json = {
 	'$': [
 		'en-US',
+		'zh-HK',
 		'zh-TW'
 	],
 	'*': {
@@ -11,10 +12,16 @@ var json = {
 		'空白': [[]],
 		'简体': [
 			'simplified',
-			'簡體'
+			'簡體',
+			1
 		]
 	},
-	'subtype': { '简体': ['simplified subtype'] }
+	'subtype': {
+		'简体': [
+			'simplified subtype',
+			'簡體'
+		]
+	}
 }
 
 }
